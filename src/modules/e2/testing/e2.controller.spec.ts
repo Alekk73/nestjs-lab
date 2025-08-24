c0874109ae27ecccfd271701d626d71c5350ee99
Alekk73 <sntxdev@gmail.com>
2025-08-24 15:35:30 -0300

    fix(Etapa 2): Aplicación de los test de service y controller

--- src/modules/e2/testing/e2.controller.spec.ts
+++ src/modules/e2/testing/e2.controller.spec.ts
@@ -1,9 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { E2Controller } from './e2.controller';
-import { E2Service } from './e2.service';
+import { E2Controller } from '../e2.controller';
+import { E2Service } from '../e2.service';
 
 describe('E2Controller', () => {
   let controller: E2Controller;
+  let service: E2Service;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +13,42 @@ describe('E2Controller', () => {
     }).compile();
 
     controller = module.get<E2Controller>(E2Controller);
+    service = module.get<E2Service>(E2Service);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Deberia devolver la lista de usaurios', () => {
+    const usersMock = [
+      { id: 1, name: 'Juan' },
+      { id: 2, name: 'Martina' },
+    ];
+
+    jest.spyOn(service, 'getAllUsers').mockImplementation(() => usersMock);
+
+    expect(controller.getAllUsers()).toEqual(usersMock);
+  });
+
+  it('Deberia devolver los datos del usuario solicitado por ID', () => {
+    const id: number = 1;
+    const userMock = { id: id, name: 'Juan' };
+    jest.spyOn(service, 'getById').mockImplementation(() => userMock);
+
+    expect(controller.getById(id)).toEqual(userMock);
+  });
+
+  it('Deberia crear un usuario', () => {
+    const body = { name: 'Maria' };
+    const result = 'Usuario creado correctamente';
+
+    jest.spyOn(service, 'create').mockImplementation(() => result);
+
+    const response = controller.create(body);
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(service.create).toHaveBeenCalledWith(body);
+    expect(response).toBe(result);
   });
 });
--- src/modules/e2/testing/e2.service.spec.ts
+++ src/modules/e2/testing/e2.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { E2Service } from './e2.service';
+import { E2Service } from '../e2.service';
 
 describe('E2Service', () => {
   let service: E2Service;
@@ -14,5 +14,35 @@ describe('E2Service', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('Deberia retornar la lista de todos los usuarios.', () => {
+    const usersMock = [
+      { id: 1, name: 'Juan' },
+      { id: 2, name: 'Martina' },
+    ];
+
+    expect(service.getAllUsers()).toEqual(usersMock);
+  });
+
+  it('Deberia retornar el usaurio solicitado', () => {
+    const id: number = 1;
+    const userMock = { id: id, name: 'Juan' };
+
+    expect(service.getById(id)).toEqual(userMock);
+  });
+
+  // Testea el funcionamiento findUser que es privada en el service
+  it('debería lanzar error si no existe el usuario', () => {
+    expect(() => service.getById(99)).toThrow(
+      'Usuario con id 99 no encontrado.',
+    );
+  });
+
+  it('Deberia guardar el nuevo usaurio y devolver un mensaje', () => {
+    const newUserMock = { name: 'Ana' };
+    const result = 'Usuario creado correctamente';
+
+    expect(service.create(newUserMock)).toBe(result);
   });
 });
